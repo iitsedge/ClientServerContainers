bd5d8f9e84911dea669c28bc1c7460ee4272c7ef
edgenicholson <edgenichollson2@gmail.com>
2020-11-02 18:40:36 -0700

    update

--- src/main/java/RestfulServer.java
+++ src/main/java/RestfulServer.java
@@ -1,14 +1,31 @@
 //import org.apache.log4j.BasicConfigurator;
+
 import spark.Request;
 import spark.Response;
 import spark.Spark;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
+import java.util.*;
 
 class RestfulServer{
+
+    public static int counter = 4; //size of deck
+    public static ArrayList<String> deck = new ArrayList<String>(){
+        {
+            add("Ace Hearts");
+            add("Ace Diamonds");
+            add("Ace Clubs");
+            add("Ace Spades");
+        }
+    };
+
+    public String getCard(){
+        Random rand = new Random();
+        int x = rand.nextInt(counter);
+        String card = deck.get(x);
+        deck.remove(x);
+        counter--;
+        return card;
+    }
     public RestfulServer(){
         this.configureRestfulApiServer();
         this.processRestfulAPIRequests();
@@ -35,11 +52,30 @@ class RestfulServer{
 
     //return all the properties of the requests coming in
     private String HttpRequestToJson(Request request){
-        System.out.println("This is my body- " + request.body());
-        return "{\n"
-                +
-                "\"body\":\"" + request.body() + "\", \n"
-                + "}";
+        String hold, hit, str;
+        hit = "You have decided to take another card!";
+        hold = "You have decided to keep your current hand!";
+        str = "Could not parse decision.";
+
+        if (request.body().equalsIgnoreCase("HIT")){
+            String card = getCard();
+            System.out.println(hit);
+            System.out.println("Card dealt: "+card);
+            System.out.println("Cards remaining in deck: "+counter);
+            System.out.println("Size of Array: "+deck.size());
+            return card;
+        }
+        else if(request.body().equalsIgnoreCase("HOLD")){
+            System.out.println(hold);
+            System.out.println("Request Body: " + request.body());
+            return hold;
+        }else{
+            System.out.println(str);
+            System.out.println("Request Body: " + request.body());
+            return str;
+        }
+        //System.out.println("This is my body- " + request.body());
+
 
     }
     public static void main(String[] argv)  {
